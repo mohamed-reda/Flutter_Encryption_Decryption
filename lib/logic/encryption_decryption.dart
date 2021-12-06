81dc388d51812ed7f6b8dd84434e486914134fea
mohamed <mohamed.reda.007007@gmail.com>
2021-12-06 09:22:59 +0200

    Update the encryption key

--- lib/logic/encryption_decryption.dart
+++ lib/logic/encryption_decryption.dart
@@ -23,7 +23,7 @@ class MyEncryptionDecryption {
 
   // For Fernet Encryption/Decryption
   static final keyFernet =
-      encrypt.Key.fromUtf8('TechWithVPIsBestTechWithVPIsBest');
+      encrypt.Key.fromUtf8('MohamedReda007007MohamedReda0070');
   // if you need to use the ttl feature, you'll need to use APIs in the algorithm itself
   static final fernet = encrypt.Fernet(keyFernet);
   static final encrypterFernet = encrypt.Encrypter(fernet);
